8ee1e4ba14d15e43e131ecd094a2079788ae0195
Ilya Dorogokupets <IlyaDorogokupets@yahoo.com>
2023-06-26 18:04:54 +0300

    25.06.23

--- src/main/java/by/dorogokupets/port/util/ShipFactory.java
+++ src/main/java/by/dorogokupets/port/util/ShipFactory.java
@@ -7,7 +7,7 @@ import by.dorogokupets.port.entity.Ship;
 public class ShipFactory {
     private static final int MAX_AMOUNT_CONTAINERS = 40;
     private int shipIdCounter = 0;
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
     public Ship createShip() {
         int currentAmountContainers = random.nextInt(MAX_AMOUNT_CONTAINERS + 1);
